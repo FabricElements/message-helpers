63f5aa414908156d2d44b7b0db5ebd88420f0993
dfchaves17 <dfchaves17@gmail.com>
2019-03-21 18:26:03 -0500

    Change names url function to validateUrl and export default function

--- src/validateUrl.ts
+++ src/validateUrl.ts
@@ -9,7 +9,7 @@
  * @param {string} url
  * @returns {string} {boolean}
  */
-export const validate = (url: string | null): string | boolean => {
+export default (url: string | null): string | boolean => {
   const baseURL = !!url ? String(url) : "";
   const cleanUrl = baseURL.replace(/\s+/g, "");
   const isURL = new RegExp("^(https|http):\/\/(w{3}.)?([a-zA-Z0-9_-]+)([.][a-zA-Z0-9_-]+)");
